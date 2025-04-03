457693a1be1f8489fe471bea08c4cbb61e9868dc
DiabloDa <jorge.callem@upb.edu.co>
2025-04-02 20:21:42 -0500

    Patrón Adapter

---
+++ ScriptingTallerTwo/Assets/scripts/Adapter.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+// Interfaz objetivo
+public interface IAdaptadorHexadecimal
+{
+    int Convertir(string numeroHex);
+}
+
+// Adaptador que convierte un número hexadecimal a int
+public class AdaptadorHexAInt : IAdaptadorHexadecimal
+{
+    public int Convertir(string numeroHex)
+    {
+        return int.Parse(numeroHex, System.Globalization.NumberStyles.HexNumber);
+    }
+}
+
+public class Adapter : MonoBehaviour
+{
+    public TextMeshProUGUI textoResultado;
+    public Button botonConvertir;
+
+    private IAdaptadorHexadecimal adaptador;
+    private string numeroHexadecimal = "1A"; // Número hexadecimal de ejemplo (26 en decimal)
+
+    void Start()
+    {
+        adaptador = new AdaptadorHexAInt();
+        botonConvertir.onClick.AddListener(ActualizarTexto);
+    }
+
+    void ActualizarTexto()
+    {
+        int numeroEntero = adaptador.Convertir(numeroHexadecimal);
+        textoResultado.text = "Número convertido: " + numeroEntero;
+    }
+}
